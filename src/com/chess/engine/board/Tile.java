c3f64bfd8ffc8a0eac498339fc683bebc0b54dfb
Unknown <locphamduc96@gmail.com>
2018-07-08 10:07:39 +0700

    Tile, Board, Piece, Move

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,8 +1,34 @@
+package com.chess.engine.board;
+
+import com.chess.engine.piece.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public abstract class Tile {
 
     int tileCoordinate;
 
-    Tile(int tileCoordinate) {
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
+
+    private static Map<Integer,EmptyTile> createAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate) {
         this.tileCoordinate = tileCoordinate;
     }
 
